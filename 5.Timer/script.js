0bc5050841af4afac5c093ef5a579175a610e1c4
Thiago Silva <59424663+ithiagosilva@users.noreply.github.com>
2024-12-15 20:52:37 -0300

    Play button timer bug fixed. Styling components to a better user experience.

--- 5.Timer/script.js
+++ 5.Timer/script.js
@@ -5,7 +5,7 @@ let zerar = document.querySelector('#zerar');
 
 let timerTag = document.createElement('h2');
 timerTag.innerText = "00:00:00";
-timerBox.insertBefore(timerTag, timerBox.childNodes[3])
+timerBox.insertBefore(timerTag, timerBox.childNodes[4])
 
 function add0(i) {
     return i < 10 ? i = "0" + i: i;
@@ -28,27 +28,34 @@ function iniciarTimer() {
     }
     timerTag.innerText = `${add0(hour)}:${add0(min)}:${add0(sec)}`;
 }
+function pararTimer() {
+    setTimeout(() => {
+        clearInterval(statusTimer);
+    });
+}
 
 let statusTimer;
+let isItOn = false;
 iniciar.addEventListener("click", () => {
-    statusTimer = setInterval(iniciarTimer, 1000)
+    if (!isItOn) {
+        statusTimer = setInterval(iniciarTimer, 1000)
+    }
+    isItOn = true;
+    timerTag.style.backgroundImage = "linear-gradient(45deg, #453ff8, #22ffc8)";
 });
 
 pausar.addEventListener("click", () => {
-    setTimeout(() => {
-        clearInterval(statusTimer)
-    });
+    pararTimer();
+    isItOn = false;
+    timerTag.style.backgroundImage = "linear-gradient(45deg,rgb(248, 63, 85),rgb(255, 34, 203))";
 });
+
 zerar.addEventListener("click", () => {
     sec = 0;
     min = 0;
     hour = 0;
     timerTag.innerText = "00:00:00";
+    pararTimer();
+    isItOn = false;
+    timerTag.style.backgroundImage = "linear-gradient(45deg,rgb(248, 63, 85),rgb(255, 34, 203))";
 });
-
-
-
-
-
-
-
